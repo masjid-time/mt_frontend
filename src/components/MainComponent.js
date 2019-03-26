04f014138d16da36caea963b0ed203ccdb89d24c
Ansuman <ansuman5555@gmail.com>
2019-03-26 11:59:58 +0530

    Included prod url for data

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -13,7 +13,7 @@ class MainComponent extends Component {
 
     async componentDidMount() {
         try {
-            this.body = await RequestPromise('http://localhost:5000/api/v1/mosques', {timeout: 5000, json: true})
+            this.body = await RequestPromise('https://prayertiming-backend.herokuapp.com/api/v1/mosques', {timeout: 5000, json: true})
             this.addressCard = await this.body.mosques.map((mosque) => <AddressCard key={mosque.id} title={mosque.name} />)
         } catch (err) {
             if (err.error.message === 'Failed to fetch') {
