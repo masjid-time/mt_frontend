016bf8da586d3bacbc025f29d772d709c6b68ee6
Ansuman <ansuman5555@gmail.com>
2019-05-04 15:51:02 +0530

    fixed footer background

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 
 class Footer extends Component {
     render() {
-        return <nav className='navbar fixed-bottom navbar-light bg-light' />;
+        return (
+            <nav className='navbar fixed-bottom navbar-light bg-transparent' />
+        );
     }
 }
 
