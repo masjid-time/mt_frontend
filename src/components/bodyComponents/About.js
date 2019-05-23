277b10d491700fb51e5f60d3af412ef5de095512
Ansuman <ansuman5555@gmail.com>
2019-05-24 00:34:35 +0530

    Changed about page to fit the card

--- src/components/bodyComponents/About.js
+++ src/components/bodyComponents/About.js
@@ -4,38 +4,30 @@ import cardImage from './../../images/background.jpg';
 class About extends Component {
     render() {
         return (
-            <div className='container'>
-                <div className='card-deck mb-0'>
-                    <div className='card mb-0 border-0 shadow-lg'>
-                        <img
-                            src={cardImage}
-                            className='card-img-top'
-                            alt='...'
-                        />
-                        <div className='card-img-overlay text-center font-weight-bold handwriting'>
-                            Worries end... When SALAH begins
-                        </div>
-                        <div className='card-body'>
-                            <h5 className='card-title text-center'>
-                                Our Mission
-                            </h5>
-                            <p className='card-text text-center'>
-                                Create an Islamic website with easy access to
-                                masjids with prayer timings so you can be worry
-                                free and have your salah at the right time and
-                                at the right place.
-                            </p>
-                            <p className='card-text text-center'>
-                                <small className='text-muted '>
-                                    Contact us:
-                                    <a
-                                        className='ml-2'
-                                        href='masjidtimeinfo@gmail.com'>
-                                        masjidtimeinfo@gmail.com
-                                    </a>
-                                </small>
-                            </p>
-                        </div>
+            <div className='container-fluid p-0 m-0 bg-white about-height'>
+                <div className='card mt-n4 mb-0 border-0'>
+                    <img src={cardImage} className='about-img' alt='...' />
+                    <div className='card-img-overlay text-center font-weight-bold handwriting'>
+                        Worries end... When SALAH begins
+                    </div>
+                    <div className='card-body about-card'>
+                        <h5 className='card-title text-center'>Our Mission</h5>
+                        <p className='card-text text-center'>
+                            Create an Islamic website with easy access to
+                            masjids with prayer timings so you can be worry free
+                            and have your salah at the right time and at the
+                            right place.
+                        </p>
+                        <p className='card-text text-center'>
+                            <small className='text-muted '>
+                                Contact us:
+                                <a
+                                    className='ml-2'
+                                    href='masjidtimeinfo@gmail.com'>
+                                    masjidtimeinfo@gmail.com
+                                </a>
+                            </small>
+                        </p>
                     </div>
                 </div>
             </div>
