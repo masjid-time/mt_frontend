f7edfdd8d74cfae49bf922ac61b5c226584cc28b
Ansuman <ansuman5555@gmail.com>
2019-05-29 00:51:13 +0530

    Added logo to About Us page

--- src/components/bodyComponents/About.js
+++ src/components/bodyComponents/About.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import cardImage from './../../images/background.jpg';
+import githubImage from './../../images/GitHub-Mark.png';
+import twitterImage from './../../images/Twitter_Logo_Blue.png';
 
 class About extends Component {
     render() {
@@ -7,7 +9,7 @@ class About extends Component {
             <div className='container-fluid p-0 m-0 bg-white about-height'>
                 <div className='card mt-n4 mb-0 border-0'>
                     <img src={cardImage} className='about-img' alt='...' />
-                    <div className='card-img-overlay text-center font-weight-bold handwriting'>
+                    <div className='card-img-overlay text-center font-weight-bold handwriting overlay-bottom'>
                         Worries end... When SALAH begins
                     </div>
                     <div className='card-body about-card'>
@@ -18,16 +20,41 @@ class About extends Component {
                             and have your salah at the right time and at the
                             right place.
                         </p>
+                        <br />
+                        <br />
+                        <br />
                         <p className='card-text text-center'>
                             <small className='text-muted '>
-                                Contact us:
-                                <a
-                                    className='ml-2'
-                                    href='masjidtimeinfo@gmail.com'>
+                                Write to us @
+                                <span className='ml-2 text-primary'>
                                     masjidtimeinfo@gmail.com
-                                </a>
+                                </span>
                             </small>
                         </p>
+                        <div className='card-text text-center logo-z-index'>
+                            <a
+                                className='text-reset text-decoration-none '
+                                target='_blank'
+                                rel='noopener noreferrer'
+                                href='https://github.com/masjid-time'>
+                                <img
+                                    className='p-2 rounded img-fluid github-logo-size'
+                                    src={githubImage}
+                                    alt='github'
+                                />
+                            </a>
+                            <a
+                                className='text-reset text-decoration-none '
+                                target='_blank'
+                                rel='noopener noreferrer'
+                                href='https://twitter.com/masjid_time'>
+                                <img
+                                    className='p-2 rounded img-fluid twitter-logo-size'
+                                    src={twitterImage}
+                                    alt='twitter'
+                                />
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
